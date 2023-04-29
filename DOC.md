637b7358820fcf31da12d1567bb813ec45ebb0ce
Antoine <al@live.be>
2023-04-29 23:17:20 +0200

    try to make an api call to sppotify

---
+++ DOC.md
@@ -0,0 +1,7 @@
+# Spotify documentation
+
+https://developer.spotify.com/documentation/web-api/howtos/web-app-profile
+
+# Nuxt useful doc
+
+https://nuxt.com/docs/examples/server/routes
---
+++ pages/spotify.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>bjr depuis nuxt v3 et typescript</h2>
+  <p>Fetched from : </p><pre>/api/spotify</pre><pre>{{ spotify }}</pre>
+</template>
+
+<script setup>
+  const { data: spotify } = await useFetch('/api/spotify');
+</script>
